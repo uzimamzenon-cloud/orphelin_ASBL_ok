62275dd724d7e35f2e90ff96a0542e63d5a7e7bf
UZIMA MATEENE ZENON <uzimamzenon@gmail.com>
2026-01-10 10:21:00 +0100

    Refactor: Renommer Config en config (minuscule)

--- Config/settings.py
+++ Config/settings.py
@@ -37,7 +37,7 @@ MIDDLEWARE = [
 ]
 
 # ⚠️ CORRECTION : Ici on met le nom EXACT de ton dossier (configuration)
-ROOT_URLCONF = 'Config.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -56,7 +56,7 @@ TEMPLATES = [
 ]
 
 # ⚠️ CORRECTION : Ici aussi
-WSGI_APPLICATION = 'Config.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 # --- BASE DE DONNÉES ---
 DATABASES = {
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn Config.wsgi
+web: gunicorn config.wsgi
